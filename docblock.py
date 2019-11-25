b9337830198872a462698e18489458b32b485ef5
Ipaskakal <ipaskakal123@gmail.com>
2019-11-25 03:14:34 +0200

    1st version parser

---
+++ docblock.py
@@ -0,0 +1,13 @@
+class Docblock:
+
+    def __init__(self, summary="", description=""):
+        self.summary = summary
+        self.description = description
+
+    def get_summary(self):
+        return self.summary
+
+    def get_description(self):
+        return self.description
+
+
